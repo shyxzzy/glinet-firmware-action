05d49c4fc314e03d71f4be76ac7f83eafce47516
shyxzzy <122173494+shyxzzy@users.noreply.github.com>
2023-01-12 23:09:13 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -80,3 +80,7 @@ else
 	make -j$(expr $(nproc) + 1)  V=s
 fi
 
+
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
